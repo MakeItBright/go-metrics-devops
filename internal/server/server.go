2f81ff7d0cbf67007d73162574334faf78c98e66
y.breslavetc <yb.apple.dev@gmail.com>
2023-04-13 21:42:04 +0300

    upd: edit model in server

--- internal/server/server.go
+++ internal/server/server.go
@@ -19,10 +19,10 @@ type server struct {
 }
 
 type Metric struct {
-	Name  string  // имя метрики
-	Type  string  // параметр, принимающий значение gauge или counter
-	Delta int64   // значение метрики в случае передачи counter
-	Value float64 // значение метрики в случае передачи gauge
+	Name  string  `json:"id"`              // имя метрики
+	Type  string  `json:"type"`            // параметр, принимающий значение gauge или counter
+	Delta int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
 }
 
 // New ...
