a1dad92eb20a0c3166d23cfff07a37c49155c714
y.breslavetc <yb.apple.dev@gmail.com>
2023-04-15 01:29:32 +0300

    upd: update flags in agent

--- internal/server/api.go
+++ internal/server/api.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/MakeItBright/go-metrics-devops/internal/storage/inmem"
 )
@@ -20,8 +21,16 @@ func init() {
 
 // Start
 func Start(config *Config) error {
+	// Приоритет параметров должен быть таким:
+	// Если указана переменная окружения, то используется она.
+	// Если нет переменной окружения, но есть аргумент командной строки (флаг), то используется он.
+	// Если нет ни переменной окружения, ни флага, то используется значение по умолчанию.
 	flag.Parse()
 	config.BindAddr = flagRunAddr
+
+	if envBindAddr := os.Getenv("ADDRESS"); envBindAddr != "" {
+		config.BindAddr = envBindAddr
+	}
 	store := inmem.New()
 
 	srv := newServer(store)
