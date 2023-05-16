ada9119c9f3d94dad4946e95a6b4127034a323b4
y.breslavetc <yb.apple.dev@gmail.com>
2023-05-16 21:41:30 +0300

    upd: edit sender

--- internal/sender/sender.go
+++ internal/sender/sender.go
@@ -2,7 +2,6 @@ package sender
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/MakeItBright/go-metrics-devops/internal/model"
 	"github.com/go-resty/resty/v2"
@@ -22,13 +21,13 @@ func NewSender(serverAddr string) *Sender {
 // SendMetrics отправляет метрики на сервер.
 func (s *Sender) SendMetrics(metrics []model.Metric) error {
 	client := resty.New()
-	client.
-		// устанавливаем количество повторений
-		SetRetryCount(3).
-		// длительность ожидания между попытками
-		SetRetryWaitTime(30 * time.Second).
-		// длительность максимального ожидания
-		SetRetryMaxWaitTime(90 * time.Second)
+	// client.
+	// устанавливаем количество повторений
+	// SetRetryCount(3).
+	// // длительность ожидания между попытками
+	// SetRetryWaitTime(30 * time.Second).
+	// // длительность максимального ожидания
+	// SetRetryMaxWaitTime(90 * time.Second)
 	for _, value := range metrics {
 
 		// url := fmt.Sprintf("%s/update/%s/%s/%s", s.url, value.Type, value.Name, value.GetValue())
